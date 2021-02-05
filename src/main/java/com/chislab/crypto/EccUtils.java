06f01f7fe14309562916557ddca3622e6a39ba59
林鹏飞 <pengfei.lin@changhong.com>
2021-02-05 11:20:05 +0800

    完善Java EccUtils的实现

--- src/main/java/com/chislab/crypto/EccUtils.java
+++ src/main/java/com/chislab/crypto/EccUtils.java
@@ -1,5 +1,6 @@
 package com.chislab.crypto;
 
+import org.apache.commons.io.FileUtils;
 import org.bouncycastle.jce.provider.BouncyCastleProvider;
 import org.bouncycastle.util.encoders.Base64;
 
@@ -7,8 +8,11 @@ import javax.crypto.BadPaddingException;
 import javax.crypto.Cipher;
 import javax.crypto.IllegalBlockSizeException;
 import javax.crypto.NoSuchPaddingException;
+import java.io.File;
+import java.io.IOException;
+import java.nio.charset.Charset;
 import java.security.*;
-import java.security.spec.ECGenParameterSpec;
+import java.security.spec.*;
 
 /**
  * @author lpf
@@ -46,6 +50,17 @@ public class EccUtils {
         return Base64.toBase64String(bytes);
     }
 
+    /**
+     *
+     * @param privateKey
+     * @param cipherText
+     * @return string
+     * @throws NoSuchPaddingException
+     * @throws NoSuchAlgorithmException
+     * @throws BadPaddingException
+     * @throws IllegalBlockSizeException
+     * @throws InvalidKeyException
+     */
     public static String eccDecrypt(PrivateKey privateKey, String cipherText) throws NoSuchPaddingException, NoSuchAlgorithmException, BadPaddingException, IllegalBlockSizeException, InvalidKeyException {
         final Cipher cipher = Cipher.getInstance("ECIES");
         cipher.init(Cipher.DECRYPT_MODE, privateKey);
@@ -53,4 +68,65 @@ public class EccUtils {
 
         return new String(bytes);
     }
+
+    /**
+     *
+     * @param keyPath
+     * @param publicKey
+     * @throws IOException
+     */
+    public static void savePublicKey(String keyPath, PublicKey publicKey) throws IOException {
+        final String data = Base64.toBase64String(publicKey.getEncoded());
+        FileUtils.writeStringToFile(new File(keyPath), data, Charset.forName("UTF-8"));
+    }
+
+    /**
+     *
+     * @param keyPath
+     * @param privateKey
+     * @throws IOException
+     */
+    public static void savePrivateKey(String keyPath, PrivateKey privateKey) throws IOException {
+        final String data = Base64.toBase64String(privateKey.getEncoded());
+        FileUtils.writeStringToFile(new File(keyPath), data, Charset.forName("UTF-8"));
+    }
+
+    /**
+     *
+     * @param keyPath
+     * @return publicKey object
+     * @throws IOException
+     */
+    public static PublicKey loadPublicKey(String keyPath) throws IOException, NoSuchProviderException, NoSuchAlgorithmException, InvalidKeySpecException {
+        final String publicKeyString = FileUtils.readFileToString(new File(keyPath), Charset.forName("UTF-8"));
+        // 创建keyFactory
+        final KeyFactory keyFactory = KeyFactory.getInstance("EC", BouncyCastleProvider.PROVIDER_NAME);
+        // 创建privateKey规则
+        final X509EncodedKeySpec x509EncodedKeySpec = new X509EncodedKeySpec(Base64.decode(publicKeyString));
+        // 获取、返回privateKey
+        final PublicKey publicKey = keyFactory.generatePublic(x509EncodedKeySpec);
+
+        return publicKey;
+    }
+
+    /**
+     *
+     * @param keyPath
+     * @return privateKey object
+     * @throws IOException
+     * @throws NoSuchAlgorithmException
+     * @throws InvalidKeySpecException
+     */
+    public static PrivateKey loadPrivateKey(String keyPath) throws IOException, NoSuchAlgorithmException, InvalidKeySpecException {
+        final String privateKeyString = FileUtils.readFileToString(new File(keyPath), Charset.forName("UTF-8"));
+        // 创建keyFactory
+        final KeyFactory keyFactory = KeyFactory.getInstance("EC");
+        // 创建privateKey规则
+        final PKCS8EncodedKeySpec pkcs8EncodedKeySpec = new PKCS8EncodedKeySpec(Base64.decode(privateKeyString));
+        // 获取、返回privateKey
+        final PrivateKey privateKey = keyFactory.generatePrivate(pkcs8EncodedKeySpec);
+
+        return privateKey;
+    }
+
 }
--- src/test/java/com/chislab/crypto/EccUtilsTest.java
+++ src/test/java/com/chislab/crypto/EccUtilsTest.java
@@ -1,12 +1,15 @@
 package com.chislab.crypto;
 
+import org.bouncycastle.util.encoders.Base64;
 import org.junit.Assert;
 import org.junit.Test;
 
 import javax.crypto.BadPaddingException;
 import javax.crypto.IllegalBlockSizeException;
 import javax.crypto.NoSuchPaddingException;
+import java.io.IOException;
 import java.security.*;
+import java.security.spec.InvalidKeySpecException;
 
 /**
  * @author lpf
@@ -15,7 +18,7 @@ import java.security.*;
 public class EccUtilsTest {
 
     @Test
-    public void testEccUtils() throws NoSuchPaddingException, InvalidKeyException, NoSuchAlgorithmException, IllegalBlockSizeException, BadPaddingException, NoSuchProviderException, InvalidAlgorithmParameterException {
+    public void testEccUtils() throws NoSuchPaddingException, InvalidKeyException, NoSuchAlgorithmException, IllegalBlockSizeException, BadPaddingException, NoSuchProviderException, InvalidAlgorithmParameterException, IOException, InvalidKeySpecException {
         final KeyPair keyPair = EccUtils.generateEccKeyPair();
         final PublicKey publicKey = keyPair.getPublic();
         final PrivateKey privateKey = keyPair.getPrivate();
@@ -24,5 +27,14 @@ public class EccUtilsTest {
         final String plainText = EccUtils.eccDecrypt(privateKey, cipherText);
 
         Assert.assertTrue("Hello Chislab".equals(plainText));
+
+        EccUtils.savePublicKey("keys/public.key", publicKey);
+        EccUtils.savePrivateKey("keys/private.key", privateKey);
+
+        final PublicKey publicKeyLoaded = EccUtils.loadPublicKey("keys/public.key");
+        final PrivateKey privateKeyLoaded = EccUtils.loadPrivateKey("keys/private.key");
+
+        System.out.println(Base64.toBase64String(publicKeyLoaded.getEncoded()));
+        System.out.println(Base64.toBase64String(privateKeyLoaded.getEncoded()));
     }
 }
